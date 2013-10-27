a5bd4d678ea72d8e33d10f13b3b13427d7d4eb45
Josh <thedataangel@gmail.com>
2013-10-27 23:40:13 +1100

    Further work on code gen. (Not working yet).

--- Base.hs
+++ Base.hs
@@ -21,8 +21,41 @@ processVarDecs (v:vs) addrs table = (instr : instrs, addrs'')
         (instr, addrs', table') = varDec v addrs table
         (instrs, addrs'') = processVarDecs vs addrs' table'
         
-       
+varDec :: VarDec -> AddressScheme -> AddressTable -> ([Instruction], AddressScheme, AddressTable)
+varDec (VarDec ident Nothing) address sym 
+    =   ([Loadi 0], 
+        (addSymbol (Identifier ident Nothing) address),
+         address {offset = (offset address) + 1)
+varDec (VarDec ident (Just e)) address sym 
+    =   (expression e sym, -- need to make sure that 'expression e' leaves its value in the right spot?
+        (addSymbol (Identifier ident Nothing) address), 
+         address {offset = (offset address) + 1))
+varDec _ _ = error "VarDec Halp"
+        
+processFunDecs :: [FunDec] -> AddressScheme -> AddressTable -> ([Instruction], AddressScheme, AddressTable)
+processFunDecs [] addr table = ([], table)
+processFunDecs (f@((FunDec id args _ _):fs) addr table = (instr ++ instrs, addr', table'')
+    where
+        table' = (addSymbol (Identifier id (length args)) addr table)
+        instr = funDec f addr table'
+        (instrs, addr', table'') 
+            = processFunDecs fs (addr {offset = (offset addr) + (length instr)}) table'
 
+funDec :: FunDec -> AddressScheme -> AddressTable -> [Instruction]
+funDec (FunDec id args vars body) addr parentTable = (Loadi 0) : varInstrs ++ bodyInstrs
+    where
+    table       = pushScope parentTable
+    argTable    = processArgs args addr table
+    (varInstrs, varTable)    = processVarDecs vars (AddressScheme 1 FP) argTable
+    (bodyInstrs, _) = (statement body addr {offset = (offset addr) + length varInstrs} varTable)
+    
+processArgs :: [String] -> AddressScheme -> AddressTable -> AddressTable
+processArgs args addr table = addSymbols keyVals table
+    where
+        addrMap = map (\x -> (AddressScheme x FP)) [-((length args) - 1) .. (-1)]
+        idMap = map (\x -> (Identifier x 0)) args
+        keyVals = ("return", -((length args) - 2)) : (zip idMap addrMap)
+        
 exp :: Exp -> AddressScheme -> AddressTable -> (AddressTable, [Instruction])
 exp TurtleData.Up _ s = (s, [PDPlot.Up])
 exp TurtleData.Down _ s = (s, [PDPlot.Down])
@@ -45,18 +78,6 @@ statement (Statements (s:ss)) addr table = (sInstr : ssInstr, addr'')
         (sInstr, addr') = statement s addr table
         (ssInstr, addr'') = statement ss addr' table
 statement (Statements []) addr _ = ([], addr)
-
-varDec :: VarDec -> AddressScheme -> AddressTable -> ([Instruction], AddressScheme, AddressTable)
-varDec (VarDec ident Nothing) address sym 
-    =   ([Loadi 0], 
-        (addSymbol (Identifier ident Nothing) address),
-         address {offset = (offset address) + 1)
-varDec (VarDec ident (Just e)) address sym 
-    =   (expression e sym, -- need to make sure that 'expression e' leaves its value in the right spot?
-        (addSymbol (Identifier ident Nothing) address), 
-         address {offset = (offset address) + 1))
-varDec _ _ = error "VarDec Halp"
-
 
 expression :: Expresssion -> AddressTable -> [Instruction]
 expression (Plus e1 e2) sym = [(expression e1 sym), (expression e2 sym) , Add]
